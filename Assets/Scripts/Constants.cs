7de657a53e5596ec00aac710217547101bee9eda
Ryan Massia [Student] <RMassia03@student.sl.on.ca>
2019-10-28 15:45:33 -0400

    chapter9: added gun and constants script

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{   // Scenes   
+    public const string SceneBattle = "Battle";  //battle scence
+    public const string SceneMenu = "MainMenu"; // main menu scence
+    // Gun Types   
+    public const string Pistol = "Pistol"; //pistal gameobject name 
+    public const string Shotgun = "Shotgun"; //shotguin gamobject name
+    public const string AssaultRifle = "AssaultRifle"; //assualt rifle gameobject name 
+    // Robot Types   
+    public const string RedRobot = "RedRobot"; //redbot gameobject name 
+    public const string BlueRobot = "BlueRobot"; //bluebot gameobject name 
+    public const string YellowRobot = "YellowRobot"; //yellow gameobject name 
+    // Pickup Types   
+    public const int PickUpPistolAmmo = 1; // pistol ammo gameobject name 
+    public const int PickUpAssaultRifleAmmo = 2; // assault rifle ammo gameobject name 
+    public const int PickUpShotgunAmmo = 3; //shotgun ammo gameobject name 
+    public const int PickUpHealth = 4; //pick up health gameobject name 
+    public const int PickUpArmor = 5;// pick up armor gameobject name 
+    // Misc   
+    public const string Game = "Game";
+    public const float CameraDefaultZoom = 60f;
+    // keeps track  of pick ups
+    public static readonly int[] AllPickupTypes = new int[5] 
+    {
+      PickUpPistolAmmo,
+      PickUpAssaultRifleAmmo,
+      PickUpShotgunAmmo,
+      PickUpHealth,
+      PickUpArmor
+    };
+}
---
+++ Assets/Scripts/GunEquipper.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunEquipper : MonoBehaviour
+{
+    public static string activeWeaponType;
+    public GameObject pistol; // pistol gameobject
+    public GameObject assaultRifle; //assault rifle gameobject
+    public GameObject shotgun; //shotgun gameobject
+    GameObject activeGun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeWeaponType = Constants.Pistol;
+        activeGun = pistol;
+
+    }
+
+    private void loadWeapon(GameObject weapon)
+    {
+        pistol.SetActive(false);
+        assaultRifle.SetActive(false);
+        shotgun.SetActive(false);
+        weapon.SetActive(true);
+        activeGun = weapon;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))  //loads the pistol and equips it
+        {
+            loadWeapon(pistol);
+            activeWeaponType = Constants.Pistol;
+        }
+        if (Input.GetKeyDown("2")) //loads the assault rifle and equips it
+        {
+            loadWeapon(assaultRifle);
+            activeWeaponType = Constants.AssaultRifle;
+        }
+        else if(Input.GetKeyDown("3"))//loads the shotgun and equips it
+        {
+            loadWeapon(shotgun);
+            activeWeaponType = Constants.Shotgun;
+        }
+    }
+
+    public GameObject GetActiveWeapon()
+   {
+        return activeGun; //returns the active gun
+    }
+}
