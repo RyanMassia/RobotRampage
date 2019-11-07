2b515682d3a42d9fe49b21107b4cd3b4570ac34a
Ryan Massia [Student] <RMassia03@student.sl.on.ca>
2019-11-07 11:31:46 -0500

    chapter 10: robots now spawn

---
+++ Assets/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+    [SerializeField]
+    RobotSpawn[] spawns; // array of spawns
+    public int enemiesLeft; // tells how many enemies left 
+
+    // Start is called before the first frame update
+    // 1 sets singleton so only 1 instance of game can exist, then calls spawn robots
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+    // 2 goes through array and spawns robots
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RobotSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] robots; // array of robot objects 
+    private int timesSpawned; // says how long robot as been spawned
+    private int healthBonus = 0; // gives health bonus
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRobot()
+    {
+        timesSpawned++;
+        healthBonus += 1 * timesSpawned;
+        GameObject robot = Instantiate(robots[Random.Range(0, robots.Length)]);
+        robot.transform.position = transform.position;
+        robot.GetComponent<Robot>().health += healthBonus;
+    }
+
+}
