269b912ff0d9fab2a4e8e58669af90cfa0c34511
Ryan Massia [Student] <RMassia03@student.sl.on.ca>
2019-11-08 13:39:23 -0500

    Chapter 11: Main Menu Created!

---
+++ Assets/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 1 loooks for the battle scence and plays it
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+    // 2 closes game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
